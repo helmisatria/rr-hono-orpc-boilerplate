df4eba66e50e8a9384f677391f6b893cab540386
Helmi Satria <satriahelmi@gmail.com>
2025-06-14 23:21:19 +0700

    worked rr + hono

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -1,29 +1,27 @@
+import { useState } from "react";
 import logoDark from "./logo-dark.svg";
 import logoLight from "./logo-light.svg";
 
 export function Welcome({ message }: { message: string }) {
+  const [number, setNumber] = useState(0);
+
+  function handleClick() {
+    setNumber(number + 2);
+  }
+
   return (
     <main className="flex items-center justify-center pt-16 pb-4">
+      <p>{number}</p>
       <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
         <header className="flex flex-col items-center gap-9">
-          <div className="w-[500px] max-w-[100vw] p-4">
-            <img
-              src={logoLight}
-              alt="React Router"
-              className="block w-full dark:hidden"
-            />
-            <img
-              src={logoDark}
-              alt="React Router"
-              className="hidden w-full dark:block"
-            />
+          <div onClick={handleClick} className="w-[500px] max-w-[100vw] p-4">
+            <img src={logoLight} alt="React Router" className="block w-full dark:hidden" />
+            <img src={logoDark} alt="React Router" className="hidden w-full dark:block" />
           </div>
         </header>
         <div className="max-w-[300px] w-full space-y-6 px-4">
           <nav className="rounded-3xl border border-gray-200 p-6 dark:border-gray-700 space-y-4">
-            <p className="leading-6 text-gray-700 dark:text-gray-200 text-center">
-              What&apos;s next?
-            </p>
+            <p className="leading-6 text-gray-700 dark:text-gray-200 text-center">What&apos;s next?</p>
             <ul>
               {resources.map(({ href, text, icon }) => (
                 <li key={href}>
--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,14 @@ import { cloudflare } from "@cloudflare/vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { reactRouterHonoServer } from "react-router-hono-server/dev";
+import { cloudflareDevProxy } from "@react-router/dev/vite/cloudflare";
 
 export default defineConfig({
   plugins: [
-    cloudflare({ viteEnvironment: { name: "ssr" } }),
+    reactRouterHonoServer({ flag: { force_react_19: true }, runtime: "cloudflare" }),
+    // cloudflare({ viteEnvironment: { name: "ssr" } }),
+    cloudflareDevProxy(),
     tailwindcss(),
     reactRouter(),
     tsconfigPaths(),
